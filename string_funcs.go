e6cccb8f6a47809f2ef6a7df09cc9f70c1967870
Raju Kadam <rajulkadam@gmail.com>
2017-03-26 23:58:57 -0700

    Adding string_funcs.go

---
+++ string_funcs.go
@@ -0,0 +1,16 @@
+package main
+
+import s "strings"
+import "fmt"
+
+var p = fmt.Println
+
+func main() {
+	p("Contains:  ", s.Contains("test", "es"))
+	p("Count:     ", s.Count("test", "t"))
+	p("HasPrefix: ", s.HasPrefix("test", "te"))
+	p("HasSuffix: ", s.HasSuffix("test", "st"))
+	p("Index:     ", s.Index("test", "e"))
+	p("Join:      ", s.Join([]string{"a", "b"}, "-"))
+	p("Repeat:    ", s.Repeat("a", 5))
+}
